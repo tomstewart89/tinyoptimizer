6aeb24dab155b7fa459b4d274d227787cf3fa860
Tom Stewart <tomstewart3107@gmail.com>
2022-01-09 10:04:35 +0900

    Add a matrix class
    * Define +-*= operators
    * Use CRTP to make operator() polymorphic
    * Add Zeros, Ones, Transpose and Reference matrix types

---
+++ matrix.h
@@ -0,0 +1,303 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+namespace tiny_sqp_solver
+{
+template <typename RefType, int Rows, int Cols = 1>
+class MatrixRef;
+
+template <typename RefType, int Rows, int Cols = 1>
+class MatrixTranspose;
+
+template <typename DerivedType, int Rows, int Cols>
+struct MatrixBase
+{
+   public:
+    constexpr static int rows = Rows;
+    constexpr static int cols = Cols;
+
+    double &operator()(int i, int j = 0)
+    {
+        assert(i < Rows && j < Cols);
+        return static_cast<DerivedType *>(this)->operator()(i, j);
+    }
+
+    double operator()(int i, int j = 0) const
+    {
+        assert(i < Rows && j < Cols);
+        return static_cast<const DerivedType *>(this)->operator()(i, j);
+    }
+
+    template <typename MatType>
+    DerivedType &operator=(const MatrixBase<MatType, Rows, Cols> &mat)
+    {
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                static_cast<DerivedType &> (*this)(i, j) = mat(i, j);
+            }
+        }
+
+        return static_cast<DerivedType &>(*this);
+    }
+
+    DerivedType &operator=(double elem)
+    {
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                static_cast<DerivedType &> (*this)(i, j) = elem;
+            }
+        }
+
+        return static_cast<DerivedType &>(*this);
+    }
+
+    template <int row_start, int row_end, int col_start, int col_end>
+    MatrixRef<DerivedType, row_end - row_start, col_end - col_start> view()
+    {
+        return MatrixRef<DerivedType, row_end - row_start, col_end - col_start>(static_cast<DerivedType &>(*this),
+                                                                                row_start, col_start);
+    }
+
+    template <int row_start, int row_end, int col_start, int col_end>
+    MatrixRef<const DerivedType, row_end - row_start, col_end - col_start> view() const
+    {
+        return MatrixRef<const DerivedType, row_end - row_start, col_end - col_start>(
+            static_cast<const DerivedType &>(*this), row_start, col_start);
+    }
+
+    MatrixTranspose<DerivedType, Cols, Rows> transpose()
+    {
+        return MatrixTranspose<DerivedType, Cols, Rows>(static_cast<DerivedType &>(*this));
+    }
+
+    MatrixTranspose<const DerivedType, Cols, Rows> transpose() const
+    {
+        return MatrixTranspose<const DerivedType, Cols, Rows>(static_cast<const DerivedType &>(*this));
+    }
+};
+
+template <int Rows, int Cols = 1>
+class Matrix : public MatrixBase<Matrix<Rows, Cols>, Rows, Cols>
+{
+   public:
+    std::array<double, Rows * Cols> storage;
+
+    double &operator()(int i, int j = 0) { return storage[i * Cols + j]; }
+    double operator()(int i, int j = 0) const { return storage[i * Cols + j]; }
+
+    Matrix() = default;
+
+    Matrix(std::initializer_list<double> init)
+    {
+        assert(init.size() == Rows * Cols);
+
+        auto it = init.begin();
+
+        for (int i = 0; i < Rows; ++i)
+        {
+            for (int j = 0; j < Cols; ++j)
+            {
+                (*this)(i, j) = *(it++);
+            }
+        }
+    }
+
+    template <typename DerivedType>
+    Matrix(const MatrixBase<DerivedType, Rows, Cols> &mat)
+    {
+        static_cast<MatrixBase<Matrix<Rows, Cols>, Rows, Cols> &>(*this) = mat;
+    }
+
+    template <typename DerivedType>
+    Matrix &operator=(const MatrixBase<DerivedType, Rows, Cols> &mat)
+    {
+        return static_cast<MatrixBase<Matrix<Rows, Cols>, Rows, Cols> &>(*this) = mat;
+    }
+};
+
+template <int Rows, int Cols = 1>
+class Zeros : public MatrixBase<Zeros<Rows, Cols>, Rows, Cols>
+{
+   public:
+    double operator()(int i, int j = 0) const { return 0.0; }
+
+    Zeros() = default;
+};
+
+template <int Rows, int Cols = 1>
+class Ones : public MatrixBase<Ones<Rows, Cols>, Rows, Cols>
+{
+   public:
+    double operator()(int i, int j = 0) const { return 1.0; }
+
+    Ones() = default;
+};
+
+template <typename RefType, int Rows, int Cols>
+class MatrixRef : public MatrixBase<MatrixRef<RefType, Rows, Cols>, Rows, Cols>
+{
+    RefType &parent_;
+    const int row_offset_;
+    const int col_offset_;
+
+   public:
+    explicit MatrixRef(RefType &parent, int row_offset = 0, int col_offset = 0)
+        : parent_(parent), row_offset_(row_offset), col_offset_(col_offset)
+    {
+    }
+
+    double &operator()(int i, int j) { return parent_(i + row_offset_, j + col_offset_); }
+    double operator()(int i, int j) const { return parent_(i + row_offset_, j + col_offset_); }
+
+    template <typename MatType>
+    MatrixRef &operator=(const MatType &mat)
+    {
+        return static_cast<MatrixBase<MatrixRef<RefType, Rows, Cols>, Rows, Cols> &>(*this) = mat;
+    }
+};
+
+template <typename RefType, int Rows, int Cols>
+class MatrixTranspose : public MatrixBase<MatrixTranspose<RefType, Rows, Cols>, Rows, Cols>
+{
+    RefType &parent_;
+
+   public:
+    explicit MatrixTranspose(RefType &parent) : parent_(parent) {}
+
+    double &operator()(int i, int j) { return parent_(j, i); }
+    double operator()(int i, int j) const { return parent_(j, i); }
+
+    template <typename MatType>
+    MatrixTranspose &operator=(const MatType &mat)
+    {
+        return static_cast<MatrixBase<MatrixTranspose<RefType, Rows, Cols>, Rows, Cols> &>(*this) = mat;
+    }
+};
+
+template <typename MatAType, typename MatBType, int MatARows, int MatACols, int MatBCols>
+Matrix<MatARows, MatBCols> operator*(const MatrixBase<MatAType, MatARows, MatACols> &matA,
+                                     const MatrixBase<MatBType, MatACols, MatBCols> &matB)
+{
+    Matrix<MatARows, MatBCols> ret;
+
+    for (int i = 0; i < MatARows; ++i)
+    {
+        for (int j = 0; j < MatBCols; ++j)
+        {
+            if (MatACols > 0)
+            {
+                ret(i, j) = matA(i, 0) * matB(0, j);
+            }
+
+            for (int k = 1; k < MatACols; k++)
+            {
+                ret(i, j) += matA(i, k) * matB(k, j);
+            }
+        }
+    }
+    return ret;
+}
+
+template <typename MatAType, typename MatBType, int Rows, int Cols>
+Matrix<Rows, Cols> operator+(const MatrixBase<MatAType, Rows, Cols> &matA, const MatrixBase<MatBType, Rows, Cols> &matB)
+{
+    Matrix<Rows, Cols> ret;
+
+    for (int i = 0; i < Rows; ++i)
+    {
+        for (int j = 0; j < Cols; ++j)
+        {
+            ret(i, j) = matA(i, j) + matB(i, j);
+        }
+    }
+
+    return ret;
+}
+
+template <typename MatAType, typename MatBType, int Rows, int Cols>
+Matrix<Rows, Cols> operator-(const MatrixBase<MatAType, Rows, Cols> &matA, const MatrixBase<MatBType, Rows, Cols> &matB)
+{
+    Matrix<Rows, Cols> ret;
+
+    for (int i = 0; i < Rows; ++i)
+    {
+        for (int j = 0; j < Cols; ++j)
+        {
+            ret(i, j) = matA(i, j) - matB(i, j);
+        }
+    }
+
+    return ret;
+}
+
+template <typename MatType, int Rows, int Cols>
+Matrix<Rows, Cols> operator*(const MatrixBase<MatType, Rows, Cols> &mat, const double k)
+{
+    Matrix<Rows, Cols> ret;
+
+    for (int i = 0; i < Rows; ++i)
+    {
+        for (int j = 0; j < Cols; ++j)
+        {
+            ret(i, j) = mat(i, j) * k;
+        }
+    }
+    return ret;
+}
+
+template <typename MatType, int Rows, int Cols>
+Matrix<Rows, Cols> operator-(const MatrixBase<MatType, Rows, Cols> &mat, const double k)
+{
+    Matrix<Rows, Cols> ret;
+
+    for (int i = 0; i < Rows; ++i)
+    {
+        for (int j = 0; j < Cols; ++j)
+        {
+            ret(i, j) = mat(i, j) - k;
+        }
+    }
+    return ret;
+}
+
+template <typename MatType, int Rows, int Cols>
+double norm(const MatrixBase<MatType, Rows, Cols> &mat)
+{
+    double sum = 0.0;
+
+    for (int i = 0; i < Rows; ++i)
+    {
+        for (int j = 0; j < Cols; ++j)
+        {
+            sum += std::pow(mat(i, j), 2);
+        }
+    }
+
+    return std::sqrt(sum);
+}
+
+template <typename DerivedType, int Rows, int Cols>
+std::ostream &operator<<(std::ostream &strm, const MatrixBase<DerivedType, Rows, Cols> &mat)
+{
+    strm << '[';
+
+    for (int i = 0; i < Rows; ++i)
+    {
+        strm << '[';
+
+        for (int j = 0; j < Cols; ++j)
+        {
+            strm << mat(i, j) << ((j == Cols - 1) ? ']' : ',');
+        }
+
+        strm << (i == Rows - 1 ? ']' : ',');
+    }
+    return strm;
+}
+}  // namespace tiny_sqp_solver
---
+++ test/test_matrix.cpp
@@ -0,0 +1,150 @@
+#include <gtest/gtest.h>
+
+#include "matrix.h"
+
+using namespace tiny_sqp_solver;
+
+TEST(Matrix, Multiplication)
+{
+    Matrix<3, 3> A = {3., 5., 8., 4., 7., 9., 2., 5., 10.};
+    Matrix<3, 3> B = {6., 3., 2., 3., 7., 5., 8., 9., 1.};
+
+    auto C = A * B;
+
+    EXPECT_DOUBLE_EQ(C(0, 0), 97.);
+    EXPECT_DOUBLE_EQ(C(0, 1), 116.);
+    EXPECT_DOUBLE_EQ(C(0, 2), 39.);
+    EXPECT_DOUBLE_EQ(C(1, 0), 117.);
+    EXPECT_DOUBLE_EQ(C(1, 1), 142.);
+    EXPECT_DOUBLE_EQ(C(1, 2), 52.);
+    EXPECT_DOUBLE_EQ(C(2, 0), 107);
+    EXPECT_DOUBLE_EQ(C(2, 1), 131.);
+    EXPECT_DOUBLE_EQ(C(2, 2), 39.);
+}
+
+TEST(Matrix, ElementwiseMultiplication)
+{
+    Matrix<3, 3> A = {3., 5., 8., 4., 7., 9., 2., 5., 10.};
+    auto B = A * 2.0;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            EXPECT_DOUBLE_EQ(B(i, j), A(i, j) * 2.0);
+        }
+    }
+}
+
+TEST(Matrix, ElementwiseSubtraction)
+{
+    Matrix<3, 3> A = {3., 5., 8., 4., 7., 9., 2., 5., 10.};
+
+    auto B = A - 5.0;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            EXPECT_DOUBLE_EQ(B(i, j), A(i, j) - 5.0);
+        }
+    }
+}
+
+TEST(Matrix, Assignment)
+{
+    Matrix<3, 3> A = {3., 5., 8., 4., 7., 9., 2., 5., 10.};
+    Matrix<3, 3> B = A;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            EXPECT_DOUBLE_EQ(A(i, j), B(i, j));
+        }
+    }
+}
+
+TEST(Matrix, Addition)
+{
+    Matrix<3, 3> A = {3., 5., 8., 4., 7., 9., 2., 5., 10.};
+    Matrix<3, 3> B = {34, 25., 18., 4.45, 7.2, 319., 32., 55., 1120.};
+
+    auto C = A + B;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            EXPECT_DOUBLE_EQ(C(i, j), A(i, j) + B(i, j));
+        }
+    }
+}
+
+TEST(Matrix, Subtraction)
+{
+    Matrix<3, 3> A = {3., 5., 8., 4., 7., 9., 2., 5., 10.};
+    Matrix<3, 3> B = {34, 25., 18., 4.45, 7.2, 319., 32., 55., 1120.};
+
+    auto C = A - B;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            EXPECT_DOUBLE_EQ(C(i, j), A(i, j) - B(i, j));
+        }
+    }
+}
+
+TEST(Matrix, OnesZeros)
+{
+    Matrix<3, 3> A = Ones<3, 3>() + Ones<3, 3>();
+    Matrix<3, 3> B = A * Zeros<3, 3>();
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            EXPECT_DOUBLE_EQ(A(i, j), 2.0);
+            EXPECT_DOUBLE_EQ(B(i, j), 0.0);
+        }
+    }
+}
+
+TEST(Matrix, Transpose)
+{
+    Matrix<3, 2> A = {3., 5., 8., 4., 7., 9.};
+    auto B = A.transpose();
+
+    EXPECT_EQ(A.rows, B.cols);
+    EXPECT_EQ(A.cols, B.rows);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 2; ++j)
+        {
+            EXPECT_DOUBLE_EQ(A(i, j), B(j, i));
+        }
+    }
+}
+
+TEST(Matrix, References)
+{
+    Matrix<3, 3> A = {3.25, 5.67, 8.67, 4.55, 7.23, 9.00, 2.35, 5.73, 10.56};
+    auto A_view = A.template view<1, 3, 1, 3>();
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            EXPECT_FLOAT_EQ(A_view(i, j), A(i + 1, j + 1));
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
